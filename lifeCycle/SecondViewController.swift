5e4178c7ca2b07c2565a8b5fac052d96eabc2b22
Poom Wutthidech <wutthidech@hotmail.com>
2021-12-19 21:25:34 +0700

    Update: add more vc to show view life cycle

---
+++ lifeCycle/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  lifeCycle
+//
+//  Created by PMJs on 19/12/2564 BE.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
